63d01215484824328710c3f5052e3313ddfc3731
RoilesGame <118464417+RoilesGame@users.noreply.github.com>
2025-11-28 18:07:33 +0300

    deleted unusual libaries

--- Assets/GameAssets/Scripts/Configs/PrintingMaterialConfig.cs
+++ Assets/GameAssets/Scripts/Configs/PrintingMaterialConfig.cs
@@ -5,40 +5,16 @@ using System;
 public class PrintingMaterialConfig : IConfig
 {
     private static string IconsPath = "Sprites/PrintingMaterials/Icons/";
-    private char delimiter = ';';
+    private char delimiter = ',';
 
     public string name;
-    public string goodPropertiesRaw;
-    public string badPropertiesRaw;
+    public string goodProperties;
+    public string badProperties;
     public string iconID;
 
     private string[] _goodProperties;
     private string[] _badProperties;
     private Sprite _icon;
-
-    public string[] GoodProperties
-    {
-        get
-        {
-            if (_goodProperties == null)
-            {
-                _goodProperties = goodPropertiesRaw.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
-            }
-            return _goodProperties;
-        }
-    }
-
-    public string[] BadProperties
-    {
-        get
-        {
-            if (_badProperties == null)
-            {
-                _badProperties = badPropertiesRaw.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
-            }
-            return _badProperties;
-        }
-    }
 
     public Sprite Icon
     {
--- Assets/GameAssets/Scripts/Shop/NewShopScripts/ShopManager.cs
+++ Assets/GameAssets/Scripts/Shop/NewShopScripts/ShopManager.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.SearchService;
 
 public class ShopManager : MonoBehaviour
 {
